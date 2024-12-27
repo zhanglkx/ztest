0c95d01f39eaffefd0e5432efede9f7b16483f11
张理科 <zhanglkx@foxmail.com>
2024-12-27 17:17:08 +0800

    1

--- JavaScript/ets.js
+++ JavaScript/ets.js
@@ -1,10 +1,36 @@
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"cardCategoryWrapper",on:{"click":_vm.navToPage}},[_c('div',{staticClass:"cardInfoWrapper",style:(_vm.item.qrcode_count === 0 ? 'padding: 8px 4px' : '')},[_c('div',{staticClass:"categoryTitle"},[_vm._v(" "+_vm._s(_vm.item.list_name || '未命名目录')+" ")]),_c('div',{staticClass:"qrcodeCount"},[_vm._v(" "+_vm._s(_vm.item.qrcode_count)+" ")]),_c('div',{staticClass:"cardIcon weui-cell__ft clifont anticon-cli-angle-right"})]),(_vm.$$getValue(_vm.item, 'first_images.length', 0) > 0)?_c('div',{staticClass:"cardImgWrapper"},_vm._l((_vm.item.first_images),function(value,index1){return _c('div',{key:index1,staticClass:"cardAllImgWrapper"},[(!_vm.item.first_image[index1])?_c('div',{staticClass:"cardIconImgWrapper",style:({
-    width: _vm.$$u(_vm.cardImgWidth),
-    height: _vm.$$u(_vm.cardImgWidth),
-  })},[_c('i',{staticClass:"clifont anticon-a-outlined-qrcode22"})]):_c('img',{staticClass:"cardIconImg",style:({
-    width: _vm.$$u(_vm.cardImgWidth),
-    height: _vm.$$u(_vm.cardImgWidth),
-  }),attrs:{"src":((_vm.item.first_image[index1]) + "?x-oss-process=image/resize,w_100/quality,Q_100/interlace,1/auto-orient,1")}})])}),0):_vm._e()])}
-var staticRenderFns = []
+import { domain } from '@/store/modules/application';
 
-export { render, staticRenderFns }
+// 定义接口类
+class NCRequest {
+  // 获取完整的上传接口地址，避免写死域名
+  static getUploadUrl() {
+    return `https://upload-api.${domain.decodeUrl}/upload.php?kid=cliim`;
+  }
+
+  /**
+   * 目前只支持h5
+   * @param file
+   */
+  static async uploadImageNew(file): Promise<any> {
+    const url = this.getUploadUrl();
+    const fm = new FormData();
+    fm.append('Filedata', file);
+
+    const fetchOption: any = {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      body: fm,
+    };
+
+    try {
+      const response = await fetch(url, fetchOption);
+      const res = await response.json();
+      return res;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+export default NCRequest;
--- Vue/test.vue
+++ Vue/test.vue
@@ -1,23 +1,67 @@
 <template>
-    <div ref="subMenuScroll" class="subMenuScroll" @scroll="loadMore">
-        <!-- 内容 -->
+    <div>
+        <h4 style="color: red;" id="head-image">Hello world!</h4>
+        <button @click="saveImage">screenshots</button>
     </div>
 </template>
 
 <script>
+import html2canvas from "html2canvas";
+import axios from "axios";
+
 export default {
-    data() {
-        return {
-            scrollTop: 0
-        }
-    },
     methods: {
-        scrollToPosition() {
-            this.$refs.subMenuScroll.scrollTo({
-                top: this.scrollTop - 4,
-                behavior: 'smooth'
-            })
+        async saveImage() {
+            const res = await this.html2canvasAndUploadFile("#head-image");
+            console.log(res);
+        },
+
+        // 传入元素id,获取服务器返回的结果
+        async html2canvasAndUploadFile(selector) {
+            const canvas = await html2canvas(document.querySelector(selector));
+            // 将canvas转成base64
+            let dataURL = canvas.toDataURL("image/png");
+
+            // 打印的是图片的base64编码
+            let blob = this.dataUrlToBlob(dataURL);
+            return await this.uploadFile(blob);
+        },
+
+        // 将base64转成图片文件流
+        dataUrlToBlob(dataUrl) {
+            let arr = dataUrl.split(","),
+                mime = arr[0].match(/:(.*?);/)[1],
+                // arr[0]是data:image/png;base64
+                bstr = atob(arr[1]),
+                n = bstr.length,
+                u8arr = new Uint8Array(n);
+
+            while (n--) {
+                u8arr[n] = bstr.charCodeAt(n);
+            }
+
+            let imgFile = new Blob([u8arr], {
+                type: mime
+            });
+            return imgFile;
+        },
+
+        // 上传二进制文件
+        async uploadFile(blob) {
+            const formData = new FormData();
+            formData.append("image", blob);
+
+            const res = await axios({
+                method: "post",
+                url: "/uploadurl",
+                data: formData,
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            });
+
+            return res.data;
         }
     }
-}
+};
 </script>
