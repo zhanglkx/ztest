76a83cee5e90cad432a810ad280499b27a55828e
张理科 <zhanglkx@foxmail.com>
2024-12-30 21:15:47 +0800

    手写一个简单的Promise 原创

--- JavaScript/case/in.js
+++ JavaScript/case/in.js
@@ -1,17 +1,43 @@
-async function getData() {
-    const data = await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('data');
-        }, 1000);
-    });
-    return data + ' processed';
+function fetchUserData() {
+    return fetch('/user')
+        .then(user => user.json())
+        .then(user => {
+            return fetch(`/profile/${user.id}`)
+                .then(profile => profile.json())
+                .then(profile => {
+                    return fetch(`/settings/${user.id}`)
+                        .then(settings => settings.json())
+                        .then(settings => {
+                            return { user, profile, settings };
+                        });
+                });
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
+function fetchUserData1() {
 
-async function getData1() {
-    const data = await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('data');
-        }, 1000);
-    })
-    return data + ' processed';
+    let user;
+    let profile;
+    let settings;
+
+    fetch('/user')
+        .then((user) => {
+            user = user;
+            return fetch(`/profile/${user.id}`)
+        })
+        .then((profile) => {
+            profile = profile;
+            return fetch(`/settings/${user.id}`)
+        })
+        .then((settings) => {
+            settings = settings;
+            return fetch(`/settings/${user.id}`)
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+
+    return { user, profile, settings };
 }
--- JavaScript/case/newTarget.js
+++ JavaScript/case/newTarget.js
@@ -1,34 +1,64 @@
-class Verhicel {
+class MyPromise {
+    // 构造方法
+    constructor(executor) {
 
-}
+        // 初始化值
+        this.initValue()
+        // 初始化this指向
+        this.initBind()
+        try {
+            // 执行传进来的函数
+            executor(this.resolve, this.reject)
+        } catch (e) {
+            // 捕捉到错误直接执行reject
+            this.reject(e)
+        }
+    }
 
-const FooMixins = (superclass) => class extends superclass {
-    foo() {
-        console.log('foo')
+    initBind() {
+        // 初始化this
+        this.resolve = this.resolve.bind(this)
+        this.reject = this.reject.bind(this)
+    }
+
+    initValue() {
+        // 初始化值
+        this.PromiseResult = null // 终值
+        this.PromiseState = 'pending' // 状态
+    }
+
+    resolve(value) {
+        // state是不可变的
+        if (this.PromiseState !== 'pending') return
+        // 如果执行resolve，状态变为fulfilled
+        this.PromiseState = 'fulfilled'
+        // 终值为传进来的值
+        this.PromiseResult = value
+    }
+
+    reject(reason) {
+        // state是不可变的
+        if (this.PromiseState !== 'pending') return
+        // 如果执行reject，状态变为rejected
+        this.PromiseState = 'rejected'
+        // 终值为传进来的reason
+        this.PromiseResult = reason
+    }
+
+    then(onFulfilled, onRejected) {
+        // 接收两个回调 onFulfilled, onRejected
+
+        // 参数校验，确保一定是函数
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val
+        onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason }
+
+        if (this.PromiseState === 'fulfilled') {
+            // 如果当前为成功状态，执行第一个回调
+            onFulfilled(this.PromiseResult)
+        } else if (this.PromiseState === 'rejected') {
+            // 如果当前为失败状态，执行第二哥回调
+            onRejected(this.PromiseResult)
+        }
+
     }
 }
-
-const BarMixins = (superclass) => class extends superclass {
-    bar() {
-        console.log('bar')
-    }
-}
-
-const BazMixins = (superclass) => class extends superclass {
-    baz() {
-        console.log('baz')
-    }
-}
-
-function mix(BaseClass, ...Mixins) {
-    return Mixins.reduce((c, mixin) => mixin(c), BaseClass)
-}
-
-class Car extends mix(Verhicel, FooMixins, BarMixins, BazMixins) {
-
-}
-
-let car = new Car()
-car.foo()
-car.bar()
-car.baz()
