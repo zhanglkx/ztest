ff115c19adc8dfb200f83756527a937268e03159
张理科 <zhanglkx@foxmail.com>
2024-12-30 20:05:32 +0800

    1

--- JavaScript/case/in.js
+++ JavaScript/case/in.js
@@ -1,31 +1,17 @@
-const person = {
-    name: 'John',
-    age: 30,
-    city: 'New York'
-};
+async function getData() {
+    const data = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('data');
+        }, 1000);
+    });
+    return data + ' processed';
+}
 
-// 错误的原型使用，应该是 Object.prototype 而不是 person.prototype
-Object.prototype.greet = function () {
-    console.log('Hello, my name is ' + this.name);
-};
-
-// // 错误的属性检查，应该检查 'greet' 而不是 'age'
-// if ('greet' in person) {
-//     console.log('The person object has a method called greet.');
-// } else {
-//     console.log('The person object does not have a method called greet.');
-// }
-
-// if ('greet' in person) {
-//     console.log('The person object has a property called age.');
-// } else {
-//     console.log('The person object does not have a property called age.');
-
-// }
-
-// 正确的属性检查，使用 hasOwnProperty 方法
-if (person.hasOwnProperty('greet')) {
-    console.log('The person object has a method called greet.');
-} else {
-    console.log('The person object does not have a method called greet.');
+async function getData1() {
+    const data = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('data');
+        }, 1000);
+    })
+    return data + ' processed';
 }
