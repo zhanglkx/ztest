2251268d5e7656ddb4ef41eb74d788d35b9b7a87
张理科 <zhanglkx@foxmail.com>
2024-12-30 20:03:40 +0800

    逆天

--- JavaScript/case/in.js
+++ JavaScript/case/in.js
@@ -4,9 +4,28 @@ const person = {
     city: 'New York'
 };
 
-if (30 in person) {
-    console.log('The person object has a property called age.');
-} else {
-    console.log('The person object does not have a property called age.');
+// 错误的原型使用，应该是 Object.prototype 而不是 person.prototype
+Object.prototype.greet = function () {
+    console.log('Hello, my name is ' + this.name);
+};
 
+// // 错误的属性检查，应该检查 'greet' 而不是 'age'
+// if ('greet' in person) {
+//     console.log('The person object has a method called greet.');
+// } else {
+//     console.log('The person object does not have a method called greet.');
+// }
+
+// if ('greet' in person) {
+//     console.log('The person object has a property called age.');
+// } else {
+//     console.log('The person object does not have a property called age.');
+
+// }
+
+// 正确的属性检查，使用 hasOwnProperty 方法
+if (person.hasOwnProperty('greet')) {
+    console.log('The person object has a method called greet.');
+} else {
+    console.log('The person object does not have a method called greet.');
 }
