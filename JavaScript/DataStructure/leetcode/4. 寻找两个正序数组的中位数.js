3e8bf1d43d4b3fc7f25ad9b348bc49597e98f3a5
张理科 <zhanglkx@foxmail.com>
2025-01-03 13:23:34 +0800

    chore: add ESLint config and update project files

--- JavaScript/DataStructure/leetcode/4. 寻找两个正序数组的中位数.js
+++ JavaScript/DataStructure/leetcode/4. 寻找两个正序数组的中位数.js
@@ -3,13 +3,13 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function (nums1, nums2) {
+const findMedianSortedArrays = function (nums1, nums2) {
 
-    const total = nums1.length + nums2.length
+    const total = nums1.length + nums2.length;
 
-    const midNum = Math.floor(total / 2)
-    let index1 = 0
-    let index2 = 0
+    const midNum = Math.floor(total / 2);
+    let index1 = 0;
+    let index2 = 0;
 
     for (let index = 0; index < total; index++) {
         const element1 = nums1[index1];
@@ -19,21 +19,21 @@ var findMedianSortedArrays = function (nums1, nums2) {
 
             if (element1 >= element2) {
                 if (index2 + 1 < nums2.length - 1) {
-                    index2++
+                    index2++;
                 } else {
-                    index1++
+                    index1++;
                 }
             } else {
 
                 if (index1 + 1 < nums1.length - 1) {
-                    index1++
+                    index1++;
                 } else {
-                    index2++
+                    index2++;
                 }
             }
 
         } else {
-            return element1 > element2 ? element2 : element1
+            return element1 > element2 ? element2 : element1;
         }
 
 
@@ -43,7 +43,7 @@ var findMedianSortedArrays = function (nums1, nums2) {
 };
 
 // let nums1 = [1, 3], nums2 = [2]
-let nums1 = [1, 2], nums2 = [3, 4]
+const nums1 = [1, 2], nums2 = [3, 4];
 
-const res = findMedianSortedArrays(nums1, nums2)
+const res = findMedianSortedArrays(nums1, nums2);
 console.log('🚀日志=====res', res);
--- JavaScript/DataStructure/leetcode/tempCodeRunnerFile.js
+++ JavaScript/DataStructure/leetcode/tempCodeRunnerFile.js
@@ -1,1 +1,33 @@
-i - j + 1
+ const total = nums1.length + nums2.length
+
+    const midNum = Math.floor(total / 2)
+    let index1 = 0
+    let index2 = 0
+
+    for (let index = 0; index < total; index++) {
+        const element1 = nums1[index1];
+        const element2 = nums2[index2];
+
+        if (index1 + index2 <= midNum) {
+
+            if (element1 >= element2) {
+                if (index2 + 1 < nums2.length - 1) {
+                    index2++
+                } else {
+                    index1++
+                }
+            } else {
+
+                if (index1 + 1 < nums1.length - 1) {
+                    index1++
+                } else {
+                    index2++
+                }
+            }
+
+        } else {
+            return element1 > element2 ? element2 : element1
+        }
+
+
+    }
