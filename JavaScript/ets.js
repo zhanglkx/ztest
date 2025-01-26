436da9cdfbf4988d0b1f93d5598cede2c08e4e05
张理科 <zhanglkx@foxmail.com>
2025-01-26 14:23:52 +0800

    提交

--- JavaScript/ets.js
+++ JavaScript/ets.js
@@ -1,2 +1,22 @@
-5白+12A+5low-E/5mm钢化/6mm钢化/8mm钢化
-+5low-
+const baseObj = {
+  a: 1,
+  b: {
+    name: '修言'
+  }
+};
+
+const newObj = new Proxy(baseObj, {
+  get(target, key, receiver) {
+    console.log('get', key);
+    return target[key];
+  },
+  set(target, key, value) {
+    console.log('set', key, value);
+    target[key] = value;
+  }
+});
+
+newObj.b.name = 2;
+newObj.b = {
+  name: '修言1'
+};
