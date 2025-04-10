c6e767f95bf1b9c49ebe60fa6211066102b29633
likezhang <zhanglkx@foxmail.com>
2025-04-10 13:42:38 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
-1;
+npm install -g @ctrip/miniTools
+
+npm ERR! code FETCH_ERROR
+npm ERR! errno FETCH_ERROR
+npm ERR! invalid json response body at https://artifactory.release.ctripcorp.com/artifactory/api/npm/trip-npm-prod/json-beautify reason: Invalid response body while trying to fetch https://artifactory.release.ctripcorp.com/artifactory/api/npm/trip-npm-prod/json-beautify: EACCES: permission denied, link '/Users/temptrip/.npm/_cacache/tmp/d7731565' -> '/Users/temptrip/.npm/_cacache/content-v2/sha512/c5/2b/203a9acafdb22aaffae5177363397f076352b310a3083b32a937a8d8ebffc36db6646f991eeeb6df13150db89f1b70772e670aed0fdba2a3dda834af2476'
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     /Users/temptrip/.npm/_logs/2025-04-07T02_24_43_723Z-debug-0.log
